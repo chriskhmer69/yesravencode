9f03cfd222343d8fc3ea51c4ff80528605d2015c
chriskhmer69 <117464543+chriskhmer69@users.noreply.github.com>
2023-05-16 12:03:39 +0200

    first commit

---
+++ YesCode-Raven-main/home/controler/home.controler.php
@@ -0,0 +1,13 @@
+$games =["Starcraft 2,"BF6", "Métro Exodus"];
+
+return $this->render('home/index')
+'truc' => "Pages d'accueil",
+"games"=>$games
+
+$ciy="gotham";
+return $$this -> render ('home/index.html.twig', [
+    'truc'=> "Page d'accueil",
+    'city' => $city
+    
+    
+    ])
